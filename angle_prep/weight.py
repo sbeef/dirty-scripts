016625a21e8f9c248528ad55b5d886c45d88751e
U-ASCHMIDT-Lab2\geomorph <stanleybeef@gmail.com>
2015-10-16 18:05:34 -0400

    weight is now junked up with all sorts of shit

--- angle_prep/weight.py
+++ angle_prep/weight.py
@@ -1,4 +1,4 @@
-import csv
+import csv, re
 
 def make_sample_dict(row_list):
     samples = {}
@@ -67,3 +67,73 @@ def load_csv(file_name):
         rows.append(row)
     f.close()
     return(rows, fields)
+
+# this function creates a "mat_dict", essentially a dictionary containing
+#   the name of the sample (to make into the .mat file name)
+#   the elemental percentages from the xrf
+#   and the density of the sample
+def make_mat_dict(density_row, xrf_row, density_name_field, density_field, xrf_name_field):
+    elements = xrf_row.keys()
+    elements.remove(xrf_name_field)
+# creates a new dictionary containing the element key and values from the xrf data
+    mat_dict = { el : xrf_rows[el] for el in elements }
+# copies over the new info from the density information
+    mat_dict[MAT_NAME_FIELD] = density_row[density_name_field]
+    mat_dict[density_field] = density_row[density_field]
+    return mat_dict
+
+def fix_id(id_str):
+    if not id_str[-1].isdigit():
+        pattern = re.compile("([0-9]+)([a-zA-Z]+)")
+        match = pattern.match(id_str)
+        num_comp = int(match.group(1))
+        alph_comp = match.group(2)
+        return "%03d%s" % (num_comp, alph_comp)
+    else:
+        return "%03d" % float(id_str)
+
+def parse_range(range_string):
+    pattern = re.compile("([0-9]+)-([0-9]+)")
+    match = pattern.match(range_string)
+    return "%s-%s" % (match.group(1), match.group(2))
+
+def parse_other(other):
+    if other[0] in (' ', '.'):
+        return "%scm" % parse_range(other[1:])
+    elif other[0] == '-':
+        return parse_range(other[1:])
+    else:
+        return other
+
+
+def xrf_name_to_name(xname):
+    if xname[0] == 'r' or xname[0].isdigit():
+        return xname
+    else:
+        exp = "([a-zA-Z]+)[- ]?([0-9]+[a-zA-Z]*)(.*)"
+        pattern = re.compile(exp)
+        match = pattern.match(xname)
+        project = match.group(1).upper()
+        sampleid = fix_id(match.group(2))
+        other = match.group(3)
+        if other:
+            end_str = parse_other(other)
+            return "%s-%s_%s" % (project, sampleid, end_str)
+        else:
+            return "%s-%s" % (project, sampleid)
+
+def dicts_to_csv(dicts, out_file, keys):
+    f = open(out_file, 'w')
+    w = csv.DictWriter(f, fieldnames=keys)
+    w.writeheader()
+    for d in dicts:
+        w.writerow(d)
+    f.close()
+
+def rename_weights(in_weight_file, out_weight_file):
+    woo = load_csv(in_weight_file)
+    weights = woo[0]
+    weight_fields = woo[1]
+    for weight in weights:
+        weight['Name'] = xrf_name_to_name(weight['Name'])
+    dicts_to_csv(weights, out_weight_file, weight_fields)
