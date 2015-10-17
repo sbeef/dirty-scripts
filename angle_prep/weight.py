d584f8cd95f68ca2df60db1b831c390fca4c4ea8
U-ASCHMIDT-Lab2\geomorph <stanleybeef@gmail.com>
2015-10-17 00:23:42 -0400

    weights is an unholy mess, it also makes mat files gahh it's disgusing!

--- angle_prep/weight.py
+++ angle_prep/weight.py
@@ -1,5 +1,18 @@
 import csv, re
 
+#this code is a mess, apologies
+
+DENSITY_FILE = "densities.csv" # a csv file of the samples and the densities.
+# the sample column should be named "Sample"
+# and the density column should be named "Density"
+
+COMPOSITION_FILE = "weighted_xrf_better_names.csv" # a csv file of the compositions from xrf data
+# the sample column should be named "Name"
+
+OUTPUT_FILE = "proper_comps_and_dens.csv" # a file to be created that has all the composition and density information
+
+# this code also creates a file "unmatched.txt" that lists all the samples in the xrf data that were not matched to density information
+
 def make_sample_dict(row_list):
     samples = {}
     for row in row_list:
@@ -68,6 +81,17 @@ def load_csv(file_name):
     f.close()
     return(rows, fields)
 
+def dicts_to_csv(dicts, out_file, keys):
+    f = open(out_file, 'w')
+    w = csv.DictWriter(f, fieldnames=keys)
+    w.writeheader()
+    for d in dicts:
+        w.writerow(d)
+    f.close()
+
+# the field of the name in the new materials output
+MAT_NAME_FIELD = "Sample Name"
+
 # this function creates a "mat_dict", essentially a dictionary containing
 #   the name of the sample (to make into the .mat file name)
 #   the elemental percentages from the xrf
@@ -76,11 +100,51 @@ def make_mat_dict(density_row, xrf_row, density_name_field, density_field, xrf_n
     elements = xrf_row.keys()
     elements.remove(xrf_name_field)
 # creates a new dictionary containing the element key and values from the xrf data
-    mat_dict = { el : xrf_rows[el] for el in elements }
+    mat_dict = { el : xrf_row[el] for el in elements }
 # copies over the new info from the density information
     mat_dict[MAT_NAME_FIELD] = density_row[density_name_field]
     mat_dict[density_field] = density_row[density_field]
     return mat_dict
+
+def match_dens_row_with_xrf(dens_row, xrf_list, density_name_field, density_field, xrf_name_field):
+    dens_name = dens_row[density_name_field]
+    xrf_names = [xrf_row['Name'] for xrf_row in xrf_list]
+    if dens_name in xrf_names:
+        xrf_row = next(row for row in xrf_list if row['Name'] == dens_name)
+        return make_mat_dict(dens_row, xrf_row, density_name_field, density_field, xrf_name_field)
+    else:
+        return None
+
+def merge_all_dens_with_xrf(dens_list, xrf_list, density_name_field, density_field, xrf_name_field):
+    merged = []
+    for dens in dens_list:
+        m = match_dens_row_with_xrf(dens, xrf_list, density_name_field, density_field, xrf_name_field)
+        if m is not None:
+            merged.append(m)
+    return merged
+
+def merge_dens_and_xrf_csv(dens_file, xrf_file, output_file, density_name_field="Sample", density_field="Density", xrf_name_field="Name"):
+    dens_l = load_csv(dens_file)
+    density_list = dens_l[0]
+    xrf_l = load_csv(xrf_file)
+    xrf_list = xrf_l[0]
+    merged = merge_all_dens_with_xrf(density_list, xrf_list, density_name_field, density_field, xrf_name_field)
+    keys = xrf_l[1]
+    keys.remove(xrf_name_field)
+    keys.insert(0, MAT_NAME_FIELD)
+    keys.append(density_field)
+    dicts_to_csv(merged, output_file, keys)
+    generate_error_report(xrf_list, merged, xrf_name_field)
+
+def generate_error_report(xrf_list, merged_list, xrf_name_field):
+    xrf_names = [row[xrf_name_field] for row in xrf_list]
+    merged_names = [row[MAT_NAME_FIELD] for row in merged_list]
+    out = open("unmatched.txt", 'w')
+    for name in xrf_names:
+        if name not in merged_names:
+            nline = "%s\n" % name
+            out.write(nline)
+    out.close()
 
 def fix_id(id_str):
     if not id_str[-1].isdigit():
@@ -122,14 +186,6 @@ def xrf_name_to_name(xname):
         else:
             return "%s-%s" % (project, sampleid)
 
-def dicts_to_csv(dicts, out_file, keys):
-    f = open(out_file, 'w')
-    w = csv.DictWriter(f, fieldnames=keys)
-    w.writeheader()
-    for d in dicts:
-        w.writerow(d)
-    f.close()
-
 def rename_weights(in_weight_file, out_weight_file):
     woo = load_csv(in_weight_file)
     weights = woo[0]
@@ -137,3 +193,41 @@ def rename_weights(in_weight_file, out_weight_file):
     for weight in weights:
         weight['Name'] = xrf_name_to_name(weight['Name'])
     dicts_to_csv(weights, out_weight_file, weight_fields)
+
+# mat file creation functions:
+def mat_sci_str(num):
+    base = "%.14E" % num
+    return "%s00%s" % (base[:-2], base[-2:])
+
+def create_element_output(el, ammount):
+    if el == "Bal":
+        el = "O"
+    fel_string = mat_sci_str(float(ammount))
+    outstring = "%s\r\n %s\r\n" % (el.upper(), fel_string)
+    return outstring
+
+def create_materials_file(sample):
+    fname = "%s_XRF_ppm.mat" % sample['Sample Name']
+    els = sample.keys()
+    els.remove('Sample Name')
+    els.remove('Density')
+    materials = [(el, sample[el]) for el in els]
+    dim = len(materials)
+    mat_file = open(fname, 'w')
+    dim_line = "%s\r\n" % dim
+    mat_file.write(dim_line)
+    for material in materials:
+        mat_file.write(create_element_output(material[0], material[1]))
+    dens_line = " %s\r\n" % mat_sci_str(float(sample['Density']))
+    mat_file.write(dens_line)
+    mat_file.close()
+
+def create_materials_files(mat_csv):
+    mats = load_csv(mat_csv)
+    mats_rows = mats[0]
+    for row in mats_rows:
+        create_materials_file(row)
+
+merge_dens_and_xrf_csv(DENSITY_FILE, COMPOSITION_FILE, OUTPUT_FILE)
+create_materials_files(OUTPUT_FILE)
+
